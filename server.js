1f9d4c641386830c5176b11759663f9681abb5e2
kumudithagimnath <kumuditharathnayake@gmail.com>
2020-08-13 21:59:19 +0530

    fix

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/users", (req, res) => {
     } else {
       // get data from remote API
       axios
-        .get("https://jsonplaceholder.typicode.com/users")
+        .get(process.env.DB_ENDPOINT)
         .then((res) => res.data)
         .then((users) => {
           // save the API response in redis store
